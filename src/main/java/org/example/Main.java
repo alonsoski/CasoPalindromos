8cdf9dd96559abd59c3a83a3688c02d2b2d5218d
alonsoski <j.alonso01@ufromail.cl>
2023-09-11 13:57:09 -0300

    1st Commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,31 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
     }
+
+    public static boolean esPalindromo(String palabraIngresada) {
+        StringBuilder palabra = new StringBuilder(palabraIngresada);
+        String palabraInvertida = palabra.reverse().toString();
+        return palabraIngresada.equals(palabraInvertida);
+    }
+    public static boolean esPalindromoMejorado(String palabraIngresada) {
+        palabraIngresada = palabraIngresada.replace(" ", "");
+        StringBuilder stringBuilderA = new StringBuilder(palabraIngresada);
+        String palabraInvertida = stringBuilderA.reverse().toString();
+        return palabraIngresada.toLowerCase().equals(palabraInvertida.toLowerCase());
+    }
+    /*
+    public static boolean esPalindromo(String cadena){
+        char[] cadenaChar= cadena.toCharArray();
+        String comparacion="";
+        for (int i = 0; i <cadenaChar.length ; i++) {
+            if(cadenaChar[i]==' '){
+
+            }else {
+                comparacion+=cadenaChar[(cadenaChar.length-1)-i];
+            }
+        }
+        return  cadena.equals(comparacion);
+    }
+    */
 }
--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -1,2 +1,16 @@
-package org.example;public class Main {
+package org.example;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.example.Main.*;
+
+public class MainTest {
+    @Test
+    public void esPalindromoTest(){
+        String[] stringIngresados={"anita lava la tina", "adaa", "", "asdSa", "asd1aa"};
+        boolean[] esperados={false, false, true, false, false};
+        for (int i = 0; i <stringIngresados.length ; i++) {
+            Assertions.assertEquals(esPalindromo(stringIngresados[i]),esperados[i]);
+        }
+    }
 }
